38fa5e7c8a1e0e461145b719dd2183c38039bc8a
bigbing <bornjre@gmail.com>
2017-10-16 04:28:37 +0545

    jenjfe

--- test05.py
+++ test05.py
@@ -10,18 +10,40 @@ screen.bgpic("board.png")
 def onclick_handler(x, y):
     print(x,y)
 
+def initial_bagh_setup():
+    keys = pos_list.keys()
+    print(screen.getshapes())
+    for key in keys:
+        if (key == 0 or key == 4 or key == 19 or key == 24):
+            [x, y] = pos_list[key]
+            tiger = "tiger.gif"
+            screen.register_shape(tiger)
+            turtle.setpos(x,y)
+            #screen.addshape(tiger)
+            turtle.shape(tiger)
+            #turtle = Turtle('tiger.gif')
 screen.onscreenclick(onclick_handler)
 X = -200
 Y = 200
+pos_list = {}
 next_x = X
 next_y = Y
 turtle.color("red")
+turtle.penup()
 for i in range(5):
     for j in range(5):
+        #turtle.tracer(0, 0)
         turtle.setpos(next_x, next_y)
-        turtle.circle(5)
+        turtle.pendown()
+        #turtle.circle(5)
+        turtle.penup()
+        key = 5 * i + j
+        pos_list[key] = [next_x, next_y] 
         next_x = next_x + 100
     next_x = X
     next_y = next_y - 100
+print(pos_list)
+initial_bagh_setup()
+turtle.speed(10)
 
 screen.mainloop()
--- test4.py
+++ test4.py
@@ -1,32 +1,10 @@
 from turtle import Turtle, Screen
-#screen = Screen()
+
 turtle = Turtle()
-screen = turtle.getscreen()
-convas = screen.getcanvas()
-#convas
-Y = screen.window_height()/2
-turtle.sety(Y)
-screen.bgcolor("orange")
-# def draw_board():
-#     turtle.color("white")
-#     turtle.setpos((100,0))
-#     turtle.color("red")
+screen1 = turtle.getscreen()
+#convas = screen.getcanvas()
 
-# draw_board()
-#screen.textinput("HEY", "Whats your name pyasi:")
-#turtle.getscreen()._root.mainloop()
-shapes = screen.getshapes()
-screen.bgpic("board.png")
-print(shapes)
-
-#start
-
-
-
-
-
-
-
+turtle.write("hahhaha)
 
 
 
